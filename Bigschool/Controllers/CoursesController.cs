2a1a80ab751639c035d0caec715d414d53f3fcfa
Vinh <gavinh953@gmail.com>
2019-03-28 11:19:59 +0700

    update

---
+++ Bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bigschool/ViewModel/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModel
+{
+    public class CourseViewModel
+    {
+        public string  Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
